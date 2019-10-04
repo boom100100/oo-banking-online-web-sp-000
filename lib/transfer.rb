b525d8acd178a845fe912ae4c29ad117b73a483a
Bernadette Davis <bsd231@nyu.edu>
2019-10-04 13:20:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,11 @@ class Transfer
   end
 
   def reverse_transfer
-    
+    @sender.balance = @sender.balance + @amount
+    @receiver.balance = @receiver.balance - @amount
+    @status = "complete"
+    #result = @status
+
   end
 
 end
