6431982197d10747fda75a753915823133a35f23
Bernadette Davis <bsd231@nyu.edu>
2019-10-04 13:18:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
 
     result = ""
 
-    if (valid? &&@status == "pending" && @sender.balance >= @amount)
+    if (valid? && @status == "pending" && @sender.balance >= @amount)
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
