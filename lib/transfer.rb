207d59788c926eea88f34e47a43495c09389d373
Bernadette Davis <bsd231@nyu.edu>
2019-10-04 13:18:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,8 +17,8 @@ class Transfer
   def execute_transaction
 
     result = ""
-    
-    if (@status == "pending" && @sender.balance >= @amount)
+
+    if (valid? &&@status == "pending" && @sender.balance >= @amount)
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
